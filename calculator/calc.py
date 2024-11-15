be0b5e946c07eae8ad70c88827e50fb95c681ffc
sulavv <57318090+sulavv@users.noreply.github.com>
2024-11-16 10:15:44 +1030

    Update calc.py

--- calculator/calc.py
+++ calculator/calc.py
@@ -37,7 +37,7 @@ def calculator():
             print("Exiting the calculator. Goodbye!")
             break
 
-        # Input Validation
+        # Validate inputs
         if choice in ['1', '2', '3', '4']:
             try:
                 # Take user input for numbers
